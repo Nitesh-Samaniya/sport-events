9f7d1ed13d6aee9542ad037b43655044a560ad6d
Nitesh-Samaiya <nitinsamaniya97@gmail.com>
2023-04-15 15:40:51 +0530

    done create user with role based

--- server/src/index.js
+++ server/src/index.js
@@ -8,11 +8,15 @@ const bodyparser = require("body-parser");
 const connect = require("./config/db");
 const PORT = process.env.PORT || 8080;
 
+const userRouter = require("./routes/user.route")
+
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(bodyparser.urlencoded({extended: false}));
 app.use(bodyparser.json());
+
+app.use("/user", userRouter);
 
 mongoose.set("strictQuery", false);
 
